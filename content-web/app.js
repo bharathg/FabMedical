7f3b8d0fd3d7c9164676d38a010bcaae77bbab3f
bharathg <gaddambharath5@gmail.com>
2022-07-22 04:44:35 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("b0aa8377-7509-4c04-ae42-a353f77c8eb8");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
